580e9e1d1bb56cdca93f391d5733f76536c59c75
praveenkumars0717 <110224590+praveenkumars0717@users.noreply.github.com>
2022-09-21 11:22:34 +0530

    Update edgeProxyCiModelOnePipeline.groovy

--- vars/edgeProxyCiModelOnePipeline.groovy
+++ vars/edgeProxyCiModelOnePipeline.groovy
@@ -108,7 +108,7 @@ def call(String branchType, String build_number) {
 
                 entityDeploymentInfos.each {
                         stage('pre-deploy-prep') {
-                            println "serviceacc : $serviceAccount"
+                            cat "$serviceAccount"
                             withCredentials([file(credentialsId: it.org, variable: 'serviceAccount')]) {
                                 echo "load api product for integration init"
                                 maven.runCommand("mvn -X package apigee-config:targetservers -Phybrid-apiproxy -Dorg=${it.org} -Denv=${it.env} -Dfile=${serviceAccount} -Dapigee.config.options=update")
